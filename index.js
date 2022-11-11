57e0be90c382eaec991741445db9a0926602dd32
Maxime Collombin <93529214+maxcollombin@users.noreply.github.com>
2022-11-11 18:27:41 +0100

    update README.md

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
@@ -14,16 +15,88 @@ app.use(
 // Route for the `GET` request on the root `/` URL
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.send('This is Node.js, Express, and Postgres API compliant with the OGC specifications')
+})
+
+// Route for the `GET` request on the  `/collections` URL
+
+app.get('/collections', (request, response, next) => {
+  var options = {
+    root: path.join(__dirname)
+};
+ 
+var fileName = 'collections.json';
+response.sendFile(fileName, options, function (err) {
+    if (err) {
+        next(err);
+    } else {
+        console.log('Sent:', fileName);
+        next();
+    }
+});
+})
+
+// Route for the `GET` request on the  `/collections/collectionID` URL
+
+app.get('/collections/images', (request, response, next) => {
+  var options = {
+    root: path.join(__dirname)
+};
+
+var fileName = 'images.json';
+response.sendFile(fileName, options, function (err) {
+    if (err) {
+        next(err);
+    } else {
+        console.log('Sent:', fileName);
+        next();
+    }
+});
+})
+
+// Route for the `GET` request on the  `/collections/collectionID/queryables` URL
+
+app.get('/collections/images/queryables', (request, response, next) => {
+  var options = {
+    root: path.join(__dirname)
+};
+
+var fileName = 'queryables.json';
+response.sendFile(fileName, options, function (err) {
+    if (err) {
+        next(err);
+    } else {
+        console.log('Sent:', fileName);
+        next();
+    }
+});
+})
+
+// Route for the `GET` request on the  `/collections/collectionID/items` URL
+
+app.get('/collections/images/items', (request, response, next) => {
+  var options = {
+    root: path.join(__dirname)
+};
+
+var fileName = 'items.json';
+response.sendFile(fileName, options, function (err) {
+    if (err) {
+        next(err);
+    } else {
+        console.log('Sent:', fileName);
+        next();
+    }
+});
 })
 
 // SET the HTTP request method, the endpoint URL path, and the relevant function for each endpoint:
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
 
 // Set the port
 
--- queries.js
+++ queries.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 })
 
 // GET all users
@@ -35,56 +35,56 @@ const getUserById = (request, response) => {
   })
 }
 
-// POST a new user
+// // POST a new user
 
-const createUser = (request, response) => {
-  const { name, email } = request.body
+// const createUser = (request, response) => {
+//   const { name, email } = request.body
 
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`User added with ID: ${results.insertId}`)
-  })
-}
+//   pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(201).send(`User added with ID: ${results.insertId}`)
+//   })
+// }
 
-// PUT updated data in an existing user
+// // PUT updated data in an existing user
 
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, email } = request.body
+// const updateUser = (request, response) => {
+//   const id = parseInt(request.params.id)
+//   const { name, email } = request.body
 
-  pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User modified with ID: ${id}`)
-    }
-  )
-}
+//   pool.query(
+//     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+//     [name, email, id],
+//     (error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(200).send(`User modified with ID: ${id}`)
+//     }
+//   )
+// }
 
-// DELETE a user
+// // DELETE a user
 
-const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
+// const deleteUser = (request, response) => {
+//   const id = parseInt(request.params.id)
 
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
+//   pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).send(`User deleted with ID: ${id}`)
+//   })
+// }
 
 // Exporting CRUD functions in a REST API
 
 module.exports = {
   getUsers,
   getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
+  // createUser,
+  // updateUser,
+  // deleteUser,
 }
